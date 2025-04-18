7273c832ea6bbf19b4dfbd7d58f46ece321de9f3
EStorfjell <64396357+EStorfjell@users.noreply.github.com>
2025-04-18 08:28:30 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Entry point for chess engine.
+ *
+ * @author Emilie Storfjell
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
